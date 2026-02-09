a8bb5957094042ad494346f46d4de5946c2ed7f0
nickpashkevich <micolas9441@gmail.com>
2026-02-09 16:28:22 +0300

    done task 2

--- tasks/task_02/solution.go
+++ tasks/task_02/solution.go
@@ -1,5 +1,11 @@
 package main
 
 func reverseRunes(s string) string {
-	// TODO
+	r := []rune(s)
+
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+
+	return string(r)
 }
