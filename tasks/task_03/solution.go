fc0958727cf89e8266da3e766b78a3a322117053
nickpashkevich <micolas9441@gmail.com>
2026-02-09 20:42:28 +0300

    done task 3

--- tasks/task_03/solution.go
+++ tasks/task_03/solution.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"errors"
+	"strconv"
+)
+
 func fizzBuzz(n int) (string, error) {
-	// TODO
+	if n < 0 {
+		return "", errors.New("input value must be a positive integer")
+	} else if n%3 == 0 && n%5 == 0 {
+		return "FizzBuzz", nil
+	} else if n%3 == 0 {
+		return "Fizz", nil
+	} else if n%5 == 0 {
+		return "Buzz", nil
+	}
+
+	return strconv.Itoa(n), nil
+
 }
